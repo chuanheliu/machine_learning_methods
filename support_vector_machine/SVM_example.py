615df0bf9dec1af58b32b9313cfa2bcea81564fd
chuanhe liu <chuanheliu@163.com>
2017-08-27 09:40:00 +0100

    support vector machine

--- support_vector_machine/SVM_example.py
+++ support_vector_machine/SVM_example.py
@@ -8,18 +8,18 @@ from sklearn import svm
 np.random.seed(0)
 
 # (20,2)-[2,2]: 20行2列矩阵，均值2方差2的正态分布，-表明在负方面，+表明在正方面
-x = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
+X = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
 
 # 给随机的点，对应label 前20个是0， 后20个是1
-y = [0] * 20 + [1] * 20
+Y = [0] * 20 + [1] * 20
 
 # 建模
-classifier = svm.SVC(kernel='liner')
-classifier.fit(x, y)
+classifier = svm.SVC(kernel='linear')
+classifier.fit(X, Y)
 
 
 # 由于模型已经建立好，实际上函数已经产生，所以以下步骤只是取出超平面的系数
-w = classifier.coef_[o]
+w = classifier.coef_[0]
 # 斜率
 a = -w[0] / w[1]
 # -5 -4 -3 .... 4 5
@@ -33,10 +33,19 @@ yy_down = a * xx + (b[1] - a * b[0])
 b = classifier.support_vectors_[-1]
 yy_up = a * xx + (b[1] - a * b[0])
 
-
+print 1
 pl.plot(xx, yy, 'k-')
+pl.plot(xx, yy_down, 'k--')
+pl.plot(xx, yy_up, 'k--')
 
+pl.scatter(classifier.support_vectors_[:, 0], classifier.support_vectors_[:, 1],
+           s=80, facecolors='none')
+pl.scatter(X[:, 0], X[:, 1], c=Y, cmap=pl.cm.Paired)
 
+pl.axis('tight')
+print 2
+pl.show()
+print 3
 
 
 
